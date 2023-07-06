50639fd65adddbfecbf9c2190e882510669ceeac
Austin1serb <128577470+Austin1serb@users.noreply.github.com>
2023-07-06 10:51:01 -0700

    JSX Components, Props, Synethic Events, and State.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,24 @@
 // Here we create the Header Component that will receive props and we know we want to have a user
 //     first and last name
-import React from 'react';
-    
+import React, {useState} from 'react';
+
 const Header = (props) => {
-    // We can assume props looks like an object literal with 2 keys and values because of what was passed in
-    // {
-    //    "firstName":"Bill",
-    //    "lastName":"Justice"
-    // }
+    const [clicks, setClicks] = useState(0)
+
+
+    const buttonClick = () => {
+
+        setClicks(clicks+ 1)
+    }
+
+
     return (
         <div>
             <h1>
                 <p className='name'>{props.lastName}, {props.firstName}</p>
-                <p>Age: {props.age}</p>
+                <p>Age: {props.age + clicks}</p>
                 <p>Haircolor: {props.hairColor}</p>
+                <button onClick={buttonClick} >Birthday for {props.lastName}, {props.firstName} </button>
             </h1>
         </div>
     );
